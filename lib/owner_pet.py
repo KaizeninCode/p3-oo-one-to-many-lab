e005e4c9440df1528bf3cdbeaf2f566e4427999f
Phil <philwekullo@gmail.com>
2024-03-07 18:44:40 +0300

    some tests not passing. code not working as intended

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,34 @@
-class Pet:
-    pass
-
 class Owner:
-    pass
+    def __init__(self, name):
+        self.name = name
+        self.pet_list = []
+
+    def pets(self):
+        return self.pet_list
+
+    def add_pet(self, pet):
+        if isinstance(pet, Pet):
+            pet.owner = self
+            self.pet_list.append(pet)
+        else:
+            raise Exception("Invalid pet type.")
+
+    def get_sorted_pets(self):
+        return sorted(self.pet_list, key=lambda pet: pet.name)
+class Pet:
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+    all = []
+
+    def __init__(self, name, pet_type, owner=None):
+        self.name = name
+        if pet_type in self.PET_TYPES:
+            self.pet_type = pet_type
+        else:
+            raise Exception("Invalid pet type.")
+        self.owner = owner
+        self.__class__.all.append(self)
+
+    @classmethod
+    def get_all(cls):
+        return cls.all
+
